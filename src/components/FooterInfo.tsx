36a90518817bf9ceb4c1cb21463908ea439020d7
amankhandelwal-dw <amankhandelwal.designoweb@gmail.com>
2025-04-15 18:43:24 +0530

    remove footer info

--- src/components/FooterInfo.tsx
+++ src/components/FooterInfo.tsx
@@ -31,31 +31,7 @@ const FooterInfo = () => {
             <a href="tel:+919261638242" className="text-white hover:underline block">+91 9261638242</a>
           </div>
 
-          {/* USA Branch Office */}
-          <div className="col-span-1">
-            <div className="flex items-center mb-3">
-              {/* <span className="inline-block w-6 h-4 bg-blue-700 mr-2 rounded-sm"></span> */}
-              <h3 className="text-lg font-bold uppercase">USA Branch Office</h3>
-            </div>
-            <address className="not-italic mb-2">
-              1111 Lexington Ave,<br />
-              Flower Mound, Texas 75028
-            </address>
-            <a href="tel:+19402795920" className="text-white hover:underline block">+1 (940) 279 9520</a>
-          </div>
-
-          {/* Netherlands Office */}
-          <div className="col-span-1">
-            <div className="flex items-center mb-3">
-              {/* <span className="inline-block w-6 h-4 bg-blue-500 mr-2 rounded-sm"></span> */}
-              <h3 className="text-lg font-bold uppercase">Netherlands Office</h3>
-            </div>
-            <address className="not-italic mb-2">
-              Rooversbroekdijk 27,<br />
-              2161 LM, Lisse, The Netherlands
-            </address>
-            <a href="tel:+31648481696" className="text-white hover:underline block">+31-648481696</a>
-          </div>
+          
         </div>
       </div>
     </section>
